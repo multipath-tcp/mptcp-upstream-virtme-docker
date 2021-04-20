9fe73fbbd435f29b98b51cfdaf02e35da9acde16
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-04-20 16:26:05 +0200

    log: fix "reset colour" code
    The previous code was valid but not understood by Cirrus. '(B' was
    printed in the logs. We don't need it here, better to remove it then.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,7 +82,7 @@ VIRTME_EXEC_POST="${KERNEL_SRC}/.virtme-exec-post"
 COLOR_RED="\E[1;31m"
 COLOR_GREEN="\E[1;32m"
 COLOR_BLUE="\E[1;34m"
-COLOR_RESET="\E(B\E[m"
+COLOR_RESET="\E[0m"
 
 # $1: color, $2: text
 print_color() {
