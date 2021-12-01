94481fc4ed6d7c73d4f4dd1522c9e668a2a235f4
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-12-01 17:37:53 +0100

    run-tests-dev: set INPUT_NO_BLOCK=1
    In dev mode, it is more common to launch a part of the tests.
    
    When you are used to have .virtme-exec-run file, it is annoying to block
    :)
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- run-tests-dev.sh
+++ run-tests-dev.sh
@@ -7,6 +7,7 @@ export DOCKER_VIRTME_NAME="virtme"
 if [[ "${-}" =~ "x" ]]; then
 	export INPUT_TRACE=1
 fi
+export INPUT_NO_BLOCK="${INPUT_NO_BLOCK:-1}"
 
 bash "-${-}" "${DIR}/build.sh"
 docker system prune --filter "label=mptcp-upstream-virtme-docker" -f
