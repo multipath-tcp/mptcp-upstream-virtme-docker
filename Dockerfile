multipath-tcp/mptcp-upstream-virtme-docker

commit c454fe249ef9e1da47a5fd75d77a61923b819267

docker: switch to Ubuntu 22.04 as new base
Instead of Ubuntu 20.04.

Please note that you might need to do a 'make clean'.

On my side, I only had to clean 'perf' build dir in .virtme.

The easiest is probably to remove the .virtme dir.

Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 LABEL name=mptcp-upstream-virtme-docker
 

commit 5b92212a70e16d0c8767858cbd81a540b1c396b1

docker: install libtraceevent for perf
Needed from Ubuntu 22.04.

Fixes: c454fe2 ("docker: switch to Ubuntu 22.04 as new base")
Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

Dockerfile
@@ -17,7 +17,7 @@ RUN apt-get update && \
 		clang lld llvm llvm-dev libcap-dev \
 		gdb crash dwarves strace \
 		iptables ebtables nftables vim psmisc bash-completion less jq \
-		gettext-base libevent-dev libnewt0.52 libslang2 libutempter0 python3-newt tmux \
+		gettext-base libevent-dev libtraceevent-dev libnewt0.52 libslang2 libutempter0 python3-newt tmux \
 		libdwarf-dev libbfd-dev libnuma-dev libzstd-dev libunwind-dev libdw-dev libslang2-dev python3-dev python3-setuptools binutils-dev libiberty-dev libbabeltrace-dev systemtap-sdt-dev libperl-dev \
 		libtap-formatter-junit-perl \
 		zstd \