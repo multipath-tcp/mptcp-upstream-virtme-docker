multipath-tcp/mptcp-upstream-virtme-docker

commit 3daf7a378ae368728e4c706f82dd6a898dab7cd4

log: add sections recognised by GitHub Actions
It will be easier to look at the logs.

While at it, also add colours for the run_loop.

Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

entrypoint.sh
@@ -14,6 +14,10 @@ is_ci() {
 	[ "${CI}" = "true" ]
 }
 
+is_github_action() {
+	[ "${GITHUB_ACTIONS}" = "true" ]
+}
+
 trace_needed() {
 	is_ci || [ "${INPUT_TRACE}" = "1" ]
 }
@@ -139,6 +143,7 @@ VIRTME_PREPARE_POST="${KERNEL_SRC}/.virtme-prepare-post"
 
 COLOR_RED="\E[1;31m"
 COLOR_GREEN="\E[1;32m"
+COLOR_YELLOW="\E[1;33m"
 COLOR_BLUE="\E[1;34m"
 COLOR_RESET="\E[0m"
 
@@ -159,7 +164,29 @@ printerr() {
 	print_color "${COLOR_RED}${*}" >&2
 }
 
+if is_github_action; then
+	# $1: description
+	log_section_start() {
+		echo -e "\n::group::${COLOR_YELLOW}${*}${COLOR_RESET}"
+	}
+
+	log_section_end() {
+		echo -e "::endgroup::\n"
+	}
+else
+	# $1: description
+	log_section_start() {
+		printinfo "${@}"
+	}
+
+	log_section_end() {
+		true
+	}
+fi
+
 setup_env() {
+	log_section_start "Setup environment"
+
 	# Avoid 'unsafe repository' error: we need to get the rev/tag later from
 	# this docker image
 	git config --global --add safe.directory "${KERNEL_SRC}"
@@ -198,6 +225,8 @@ setup_env() {
 	TESTS_SUMMARY="${RESULTS_DIR}/summary.txt"
 	CONCLUSION="${RESULTS_DIR}/conclusion.txt"
 	KMEMLEAK="${RESULTS_DIR}/kmemleak.txt"
+
+	log_section_end
 }
 
 _get_last_iproute_version() {
@@ -219,7 +248,7 @@ check_last_iproute() { local last curr
 		return 0
 	fi
 
-	printinfo "Check IPRoute2 version"
+	log_section_start "Check IPRoute2 version"
 
 	last="$(_get_last_iproute_version)"
 
@@ -235,6 +264,7 @@ check_last_iproute() { local last curr
 		exit 1
 	fi
 
+	log_section_end
 }
 
 _check_source_exec_one() {
@@ -255,11 +285,13 @@ _check_source_exec_one() {
 }
 
 check_source_exec_all() {
-	printinfo "Check extented exec files"
+	log_section_start "Check extented exec files"
 
 	_check_source_exec_one "${VIRTME_EXEC_PRE}" "before the tests suite"
 	_check_source_exec_one "${VIRTME_EXEC_RUN}" "to replace the execution of the whole tests suite"
 	_check_source_exec_one "${VIRTME_EXEC_POST}" "after the tests suite"
+
+	log_section_end
 }
 
 _make() {
@@ -288,7 +320,7 @@ gen_kconfig() { local mode kconfig=()
 	mode="${1}"
 	shift
 
-	printinfo "Generate kernel config"
+	log_section_start "Generate kernel config"
 
 	if [ "${mode}" = "debug" ]; then
 		kconfig+=(
@@ -381,9 +413,13 @@ gen_kconfig() { local mode kconfig=()
 		# Useful to help reproducing issues later
 		zstd -19 -T0 "${VIRTME_KCONFIG}" -o "${RESULTS_DIR}/config.zstd"
 	fi
+
+	log_section_end
 }
 
 build_kernel() {
+	log_section_start "Build kernel"
+
 	# undo BPFTrace and cie workaround
 	find "${VIRTME_BUILD_DIR}/include" \
 		-mindepth 1 -maxdepth 1 \
@@ -392,30 +428,47 @@ build_kernel() {
 
 	_make_o
 
+	log_section_end
+}
+
+install_kernel_headers() {
+	log_section_start "Install kernel headers"
+
 	# for BPFTrace and cie
 	cp -r include/ "${VIRTME_BUILD_DIR}"
 
 	_make_o headers_install INSTALL_HDR_PATH="${VIRTME_BUILD_DIR}"
+
+	log_section_end
+
 }
 
 build_modules() {
+	log_section_start "Build kernel modules"
+
 	_make_o modules
 
 	# virtme will mount a tmpfs there + symlink to .virtme_mods
 	mkdir -p /lib/modules
+
+	log_section_end
 }
 
 build_perf() {
 	if [ "${INPUT_BUILD_SKIP_PERF}" = 1 ]; then
-		printinfo "Skip perf build"
+		printinfo "Skip Perf build"
 		return 0
 	fi
+
+	log_section_start "Build Perf"
 
 	cd tools/perf
 
 	_make O="${VIRTME_PERF_DIR}" DESTDIR=/usr install
 
 	cd "${KERNEL_SRC}"
+
+	log_section_end
 }
 
 build() {
@@ -424,9 +477,8 @@ build() {
 		return 0
 	fi
 
-	printinfo "Build the kernel"
-
 	build_kernel
+	install_kernel_headers
 	build_modules
 	build_perf
 }
@@ -437,7 +489,11 @@ build_selftests() {
 		return 0
 	fi
 
+	log_section_start "Build the selftests $(basename "${SELFTESTS_DIR}")"
+
 	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${SELFTESTS_DIR}"
+
+	log_section_end
 }
 
 build_bpftests() {
@@ -446,7 +502,11 @@ build_bpftests() {
 		return 0
 	fi
 
+	log_section_start "Build BPFTests"
+
 	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${BPFTESTS_DIR}"
+
+	log_section_end
 }
 
 build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
@@ -454,6 +514,8 @@ build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
 		printinfo "Skip Packetdrill build"
 		return 0
 	fi
+
+	log_section_start "Build Packetdrill"
 
 	old_pwd="${PWD}"
 
@@ -523,6 +585,8 @@ build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
 		set_trace_on
 	fi
 	cd "${old_pwd}"
+
+	log_section_end
 }
 
 prepare_hosts_file() {
@@ -574,6 +638,25 @@ set_max_threads() {
 	fi
 }
 
+if [ "${GITHUB_ACTIONS}" = "true" ]; then
+	# \$1: description
+	log_section_start() {
+		echo -e "\n::group::${COLOR_YELLOW}\${*}${COLOR_RESET}"
+	}
+
+	log_section_end() {
+		echo -e "::endgroup::\n"
+	}
+else
+	# \$1: description
+	log_section_start() {
+		echo -e "${COLOR_BLUE}\${*}${COLOR_RESET}"
+	}
+
+	log_section_end() {
+		true
+	}
+fi
 
 # \$1: name of the test
 _can_run() { local tname
@@ -705,10 +788,14 @@ run_kunit_one() { local ko kunit kunit_path
 	kunit="\${kunit//_/-}" # dash
 	kunit_path="/sys/kernel/debug/kunit/\${kunit}/results"
 
+	log_section_start "KUnit Test: \${kunit}"
+
 	run_kunit_core || return \${?}
 
 	insmod "\${ko}" || true # errors will also be visible below: no results
 	_kunit_result "\${kunit_path}" "\${kunit}" | tee "${RESULTS_DIR}/kunit_\${kunit}.tap"
+
+	log_section_end
 }
 
 run_kunit_all() { local ko rc=0
@@ -737,7 +824,9 @@ run_selftest_one() { local sf tap
 
 	_can_run "\${tap}" || return 0
 
+	log_section_start "Selftest Test: \${sf}"
 	_run_selftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${sf}" "\${@}"
+	log_section_end
 }
 
 run_selftest_all() { local sf rc=0
@@ -777,9 +866,11 @@ run_packetdrill_one() { local pktd_dir pktd tap
 
 	_can_run "\${tap}" || return 0
 
+	log_section_start "Packetdrill Test: \${pktd}"
 	cd /opt/packetdrill/gtests/net/
 	PYTHONUNBUFFERED=1 _tap "${RESULTS_DIR}/\${tap}" \
 		./packetdrill/run_all.py -l -v -P \${MAX_THREADS} \${pktd_dir}
+	log_section_end
 }
 
 run_packetdrill_all() { local pktd_dir rc=0
@@ -810,7 +901,9 @@ run_bpftest_one() { local bf bt tap
 
 	_can_run "\${tap}" || return 0
 
+	log_section_start "BPF Test: \${bf} -t \${bt}"
 	_run_bpftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${bf}" -t "\${bt}"
+	log_section_end
 }
 
 run_bpftest_all() {
@@ -861,12 +954,12 @@ run_loop_n() { local i tdir rc=0
 
 	i=1
 	while true; do
-		echo -e "\n\n\t=== Attempt: \${i} (\$(date -R)) ===\n\n"
+		echo -e "\n\n\t=== ${COLOR_BLUE}Attempt: \${i} (\$(date -R))${COLOR_RESET} ===\n\n"
 
 		if ! "\${@}" || has_call_trace; then
 			rc=1
 
-			echo -e "\n\n\t=== ERROR after \${i} attempts (\$(date -R)) ===\n\n"
+			echo -e "\n\n\t=== ${COLOR_RED}ERROR after \${i} attempts (\$(date -R))${COLOR_RESET} ===\n\n"
 
 			if [ "${INPUT_RUN_LOOP_CONTINUE}" = "1" ]; then
 				echo "Attempt: \${i}" >> "${CONCLUSION}.failed"
@@ -884,7 +977,7 @@ run_loop_n() { local i tdir rc=0
 		i=\$((i+1))
 	done
 
-	echo -e "\n\n\tStopped after \${i} attempts\n\n"
+	echo -e "\n\n\t${COLOR_BLUE}Stopped after \${i} attempts${COLOR_RESET}\n\n"
 
 	return "\${rc}"
 }
@@ -908,7 +1001,7 @@ fi
 
 # To exec different tests than the full suite
 if [ -f "${VIRTME_EXEC_RUN}" ]; then
-	echo -e "\n\n\tNot running all tests but:\n\n-------- 8< --------\n\$(sed "s/#.*//g;/^\s*$/d" "${VIRTME_EXEC_RUN}")\n-------- 8< --------\n\n"
+	echo -e "\n\n\t${COLOR_YELLOW}Not running all tests but:${COLOR_RESET}\n\n-------- 8< --------\n\$(sed "s/#.*//g;/^\s*$/d" "${VIRTME_EXEC_RUN}")\n-------- 8< --------\n\n"
 	source "${VIRTME_EXEC_RUN}"
 	# e.g.:
 	# run_selftest_one ./mptcp_join.sh -f
@@ -965,6 +1058,7 @@ run_expect() {
 
 	cat <<EOF > "${VIRTME_RUN_SCRIPT}"
 #! /bin/bash -x
+echo -e "$(log_section_start "Boot VM")"
 "${VIRTME_RUN}" ${VIRTME_RUN_OPTS[@]} 2>&1 | tr -d '\r'
 EOF
 	chmod +x "${VIRTME_RUN_SCRIPT}"
@@ -979,9 +1073,11 @@ expect {
 		send_user "Waiting for the console to be ready\n"
 		send "\r"
 	} timeout {
+		send_user "\n$(log_section_end)"
 		send_user "Timeout ttyS0: stopping\n"
 		exit 1
 	} eof {
+		send_user "\n$(log_section_end)"
 		send_user "${VIRTME_SCRIPT_UNEXPECTED_STOP} (ttyS0)\n"
 		exit 1
 	}
@@ -992,12 +1088,14 @@ set timeout "1"
 for {set i 0} {\$i < 60} {incr i 1} {
 	expect {
 		"root@" {
+			send_user "\n$(log_section_end)"
 			send_user "Starting the validation script (after \$i sec)\n"
 			break
 		} timeout {
 			sleep 1
 			send "\r"
 		} eof {
+			send_user "\n$(log_section_end)"
 			send_user "${VIRTME_SCRIPT_UNEXPECTED_STOP} (console)\n"
 			exit 1
 		}
@@ -1005,6 +1103,7 @@ for {set i 0} {\$i < 60} {incr i 1} {
 }
 
 if {\$i >= 60} {
+	send_user "\n$(log_section_end)"
 	send_user "Timeout console: stopping (\$i)\n"
 	exit 1
 }
@@ -1016,6 +1115,7 @@ expect {
 	"${VIRTME_SCRIPT_END}\r" {
 		send_user "validation script ended with success\n"
 	} timeout {
+		send_user "\n$(log_section_end)"
 		send_user "Timeout: sending Ctrl+C\n"
 		send "\x03\r"
 		sleep 2
@@ -1042,7 +1142,9 @@ _get_selftests_gen_files() {
 
 ccache_stat() {
 	if is_ci; then
+		log_section_start "CCache Stats"
 		ccache -s
+		log_section_end
 	fi
 }
 

commit 07d72c6960c915c72e6ce46baed2691a1c24b9f7

log: correctly catch errors as before
Because of the new sections overriding the implicit returned value, the
errors were no longer caught as before when using 'run_loop' helpers. We
were still catching tests errors because this checks the TAP output.

We need to propagate the errors. A bit more verbose but more explicit.

Fixes: 3daf7a3 ("log: add sections recognised by GitHub Actions")
Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

entrypoint.sh
@@ -316,7 +316,7 @@ _add_symlink() {
 }
 
 # $@: extra kconfig
-gen_kconfig() { local mode kconfig=()
+gen_kconfig() { local mode kconfig=() rc=0
 	mode="${1}"
 	shift
 
@@ -330,13 +330,13 @@ gen_kconfig() { local mode kconfig=()
 			-e BOOTPARAM_HUNG_TASK_PANIC # instead of blocking
 		)
 
-		_make_o defconfig debug.config
+		_make_o defconfig debug.config || rc=${?}
 	else
 		# low-overhead sampling-based memory safety error detector.
 		# Only in non-debug: KASAN is more precise
 		kconfig+=(-e KFENCE)
 
-		_make_o defconfig "${VIRTME_ARCH}_defconfig"
+		_make_o defconfig "${VIRTME_ARCH}_defconfig" || rc=${?}
 	fi
 
 	# Reboot the VM instead of blocking in case of panic
@@ -400,24 +400,26 @@ gen_kconfig() { local mode kconfig=()
 	kconfig+=("${@}")
 
 	# KBUILD_OUTPUT is used by virtme
-	"${VIRTME_CONFIGKERNEL}" --arch "${VIRTME_ARCH}" --update
+	"${VIRTME_CONFIGKERNEL}" --arch "${VIRTME_ARCH}" --update || rc=${?}
 
 	# Extra options are needed for kselftests
-	./scripts/kconfig/merge_config.sh -m "${VIRTME_KCONFIG}" "${SELFTESTS_CONFIG}"
+	./scripts/kconfig/merge_config.sh -m "${VIRTME_KCONFIG}" "${SELFTESTS_CONFIG}" || rc=${?}
 
-	./scripts/config --file "${VIRTME_KCONFIG}" "${kconfig[@]}"
+	./scripts/config --file "${VIRTME_KCONFIG}" "${kconfig[@]}" || rc=${?}
 
-	_make_o olddefconfig
+	_make_o olddefconfig || rc=${?}
 
 	if is_ci; then
 		# Useful to help reproducing issues later
-		zstd -19 -T0 "${VIRTME_KCONFIG}" -o "${RESULTS_DIR}/config.zstd"
+		zstd -19 -T0 "${VIRTME_KCONFIG}" -o "${RESULTS_DIR}/config.zstd" || rc=${?}
 	fi
 
 	log_section_end
+
+	return ${rc}
 }
 
-build_kernel() {
+build_kernel() { local rc=0
 	log_section_start "Build kernel"
 
 	# undo BPFTrace and cie workaround
@@ -426,35 +428,41 @@ build_kernel() {
 		! -name 'config' ! -name 'generated' \
 		-type d -exec rm -r {} +
 
-	_make_o
+	_make_o || rc=${?}
 
 	log_section_end
+
+	return ${rc}
 }
 
-install_kernel_headers() {
+install_kernel_headers() { local rc=0
 	log_section_start "Install kernel headers"
 
 	# for BPFTrace and cie
 	cp -r include/ "${VIRTME_BUILD_DIR}"
 
-	_make_o headers_install INSTALL_HDR_PATH="${VIRTME_BUILD_DIR}"
+	_make_o headers_install INSTALL_HDR_PATH="${VIRTME_BUILD_DIR}" || rc=${?}
 
 	log_section_end
 
+	return ${rc}
+
 }
 
-build_modules() {
+build_modules() { local rc=0
 	log_section_start "Build kernel modules"
 
-	_make_o modules
+	_make_o modules || rc=${?}
 
 	# virtme will mount a tmpfs there + symlink to .virtme_mods
 	mkdir -p /lib/modules
 
 	log_section_end
+
+	return ${rc}
 }
 
-build_perf() {
+build_perf() { local rc=0
 	if [ "${INPUT_BUILD_SKIP_PERF}" = 1 ]; then
 		printinfo "Skip Perf build"
 		return 0
@@ -464,11 +472,13 @@ build_perf() {
 
 	cd tools/perf
 
-	_make O="${VIRTME_PERF_DIR}" DESTDIR=/usr install
+	_make O="${VIRTME_PERF_DIR}" DESTDIR=/usr install || rc=${?}
 
 	cd "${KERNEL_SRC}"
 
 	log_section_end
+
+	return ${rc}
 }
 
 build() {
@@ -483,7 +493,7 @@ build() {
 	build_perf
 }
 
-build_selftests() {
+build_selftests() { local rc=0
 	if [ "${INPUT_BUILD_SKIP_SELFTESTS}" = 1 ]; then
 		printinfo "Skip selftests build"
 		return 0
@@ -491,12 +501,14 @@ build_selftests() {
 
 	log_section_start "Build the selftests $(basename "${SELFTESTS_DIR}")"
 
-	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${SELFTESTS_DIR}"
+	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${SELFTESTS_DIR}" || rc=${?}
 
 	log_section_end
+
+	return ${rc}
 }
 
-build_bpftests() {
+build_bpftests() { local rc=0
 	if [ "${INPUT_BUILD_SKIP_BPFTESTS}" = 1 ]; then
 		printinfo "Skip bpftests build"
 		return 0
@@ -504,12 +516,14 @@ build_bpftests() {
 
 	log_section_start "Build BPFTests"
 
-	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${BPFTESTS_DIR}"
+	_make_o KHDR_INCLUDES="-I${VIRTME_BUILD_DIR}/include" -C "${BPFTESTS_DIR}" || rc=${?}
 
 	log_section_end
+
+	return ${rc}
 }
 
-build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
+build_packetdrill() { local old_pwd kversion kver_maj kver_min branch rc=0
 	if [ "${INPUT_BUILD_SKIP_PACKETDRILL}" = 1 ]; then
 		printinfo "Skip Packetdrill build"
 		return 0
@@ -555,7 +569,7 @@ build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
 	fi
 	cd gtests/net/packetdrill/
 	./configure
-	_make
+	_make || rc=${?}
 
 	cd ../mptcp
 	if [ "${INPUT_PACKETDRILL_NO_MORE_TOLERANCE}" = "1" ]; then
@@ -587,6 +601,8 @@ build_packetdrill() { local old_pwd kversion kver_maj kver_min branch
 	cd "${old_pwd}"
 
 	log_section_end
+
+	return ${rc}
 }
 
 prepare_hosts_file() {
@@ -780,7 +796,7 @@ run_kunit_core() {
 }
 
 # \$1: .ko path
-run_kunit_one() { local ko kunit kunit_path
+run_kunit_one() { local ko kunit kunit_path rc=0
 	ko="\${1}"
 
 	kunit="\${ko#${VIRTME_BUILD_DIR}/}" # remove abs dir
@@ -793,9 +809,11 @@ run_kunit_one() { local ko kunit kunit_path
 	run_kunit_core || return \${?}
 
 	insmod "\${ko}" || true # errors will also be visible below: no results
-	_kunit_result "\${kunit_path}" "\${kunit}" | tee "${RESULTS_DIR}/kunit_\${kunit}.tap"
+	_kunit_result "\${kunit_path}" "\${kunit}" | tee "${RESULTS_DIR}/kunit_\${kunit}.tap" || rc=\${?}
 
 	log_section_end
+
+	return \${rc}
 }
 
 run_kunit_all() { local ko rc=0
@@ -812,21 +830,23 @@ run_kunit_all() { local ko rc=0
 
 # \$1: output tap file; rest: command to launch
 _run_selftest_one_tap() {
-	log_section_start "Selftest Test: \${*:2}"
 	cd "${KERNEL_SRC}/${SELFTESTS_DIR}"
 	_tap "\${@}"
-	log_section_end
 }
 
 # \$1: script file; rest: command to launch
-run_selftest_one() { local sf tap
+run_selftest_one() { local sf tap rc=0
 	sf=\$(basename \${1})
 	tap=selftest_\${sf:0:-3}
 	shift
 
 	_can_run "\${tap}" || return 0
 
-	_run_selftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${sf}" "\${@}"
+	log_section_start "Selftest Test: ./\${sf}\${1:+ \${*}}"
+	_run_selftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${sf}" "\${@}" || rc=\${?}
+	log_section_end
+
+	return \${rc}
 }
 
 run_selftest_all() { local sf rc=0
@@ -842,10 +862,14 @@ run_selftest_all() { local sf rc=0
 	return \${rc}
 }
 
-_run_mptcp_connect_opt() { local t="mptcp_connect_\${1}"
+_run_mptcp_connect_opt() { local t="mptcp_connect_\${1}" rc=0
 	shift
 
-	MPTCP_LIB_KSFT_TEST=\${t} _run_selftest_one_tap "${RESULTS_DIR}/\${t}" ./mptcp_connect.sh "\${@}"
+	log_section_start "Selftest Test: ./mptcp_connect.sh \${*}"
+	MPTCP_LIB_KSFT_TEST=\${t} _run_selftest_one_tap "${RESULTS_DIR}/\${t}" ./mptcp_connect.sh "\${@}" || rc=\${?}
+	log_section_end
+
+	return \${rc}
 }
 
 run_mptcp_connect_mmap() {
@@ -855,7 +879,7 @@ run_mptcp_connect_mmap() {
 }
 
 # \$1: pktd_dir (e.g. mptcp/dss)
-run_packetdrill_one() { local pktd_dir pktd tap
+run_packetdrill_one() { local pktd_dir pktd tap rc=0
 	pktd_dir="\${1}"
 	pktd="\${pktd_dir#*/}"
 	tap="packetdrill_\${pktd//\//_}"
@@ -869,8 +893,10 @@ run_packetdrill_one() { local pktd_dir pktd tap
 	log_section_start "Packetdrill Test: \${pktd}"
 	cd /opt/packetdrill/gtests/net/
 	PYTHONUNBUFFERED=1 _tap "${RESULTS_DIR}/\${tap}" \
-		./packetdrill/run_all.py -l -v -P \${MAX_THREADS} \${pktd_dir}
+		./packetdrill/run_all.py -l -v -P \${MAX_THREADS} \${pktd_dir} || rc=\${?}
 	log_section_end
+
+	return \${rc}
 }
 
 run_packetdrill_all() { local pktd_dir rc=0
@@ -894,7 +920,7 @@ _run_bpftest_one_tap() {
 }
 
 # \$1: script file; rest: command to launch
-run_bpftest_one() { local bf bt tap
+run_bpftest_one() { local bf bt tap rc=0
 	bf=\$(basename \${1})
 	bt=\${2}
 	tap=bpftest_\${bf}_\${bt}
@@ -902,8 +928,10 @@ run_bpftest_one() { local bf bt tap
 	_can_run "\${tap}" || return 0
 
 	log_section_start "BPF Test: \${bf} -t \${bt}"
-	_run_bpftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${bf}" -t "\${bt}"
+	_run_bpftest_one_tap "${RESULTS_DIR}/\${tap}" "./\${bf}" -t "\${bt}" || rc=\${?}
 	log_section_end
+
+	return \${rc}
 }
 
 run_bpftest_all() {