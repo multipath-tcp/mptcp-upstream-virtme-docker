b7de2f739492d996ab303fe75fde3e21c042b87b
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-05-01 20:33:42 +0200

    build: use docker buildx
    The new recommended way (and probably the default one).
    
    So we can pass buildx specific args later.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 # SPDX-License-Identifier: GPL-2.0
 cd "$(dirname "$(realpath -P "${0}")")"
-docker build -t "${DOCKER_VIRTME_NAME:-mptcp/mptcp-upstream-virtme-docker:latest}" -f Dockerfile .
+docker buildx build -t "${DOCKER_VIRTME_NAME:-mptcp/mptcp-upstream-virtme-docker:latest}" -f Dockerfile .
