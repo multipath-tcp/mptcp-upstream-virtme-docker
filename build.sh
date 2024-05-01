0167f69d10270fea09ed80f1f57a0e0c938375ea
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-05-01 20:34:56 +0200

    build: use --progress 'plain' in debug
    Not to loose info.
    
    In non debug mode, try to be quite.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -ex
 # SPDX-License-Identifier: GPL-2.0
+
 cd "$(dirname "$(realpath -P "${0}")")"
-docker buildx build -t "${DOCKER_VIRTME_NAME:-mptcp/mptcp-upstream-virtme-docker:latest}" -f Dockerfile .
+
+ARGS=(
+	-t "${DOCKER_VIRTME_NAME:-mptcp/mptcp-upstream-virtme-docker:latest}"
+	-f Dockerfile
+)
+
+if [[ "${-}" =~ "x" ]]; then
+	ARGS+=(--progress plain)
+else
+	echo "Building Docker image" >&2
+	ARGS+=(--quiet)
+fi
+
+docker buildx build "${ARGS[@]}" "${@}" .
