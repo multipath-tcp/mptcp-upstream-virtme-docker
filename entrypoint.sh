9065bef62c0e566e476e898d1631e959c232526e
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-10-16 13:13:13 +0200

    kconfig: enable PROVE_RCU_LIST
    virtme-configkernel will do a 'make olddefconfig', which will remove
    PROVE_RCU_LIST. We then need to explicitly enable it again after.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -401,7 +401,7 @@ gen_kconfig() { local mode kconfig=() vck rc=0
 			-d SLUB_DEBUG_ON # perf impact is too important
 			-e BOOTPARAM_SOFTLOCKUP_PANIC # instead of blocking
 			-e BOOTPARAM_HUNG_TASK_PANIC # instead of blocking
-			-e CONFIG_RCU_EXPERT # required for PROVE_RCU_LIST
+			-e RCU_EXPERT -e PROVE_RCU_LIST # fixed in v6.12
 		)
 
 		local debug_config="kernel/configs/debug.config"
