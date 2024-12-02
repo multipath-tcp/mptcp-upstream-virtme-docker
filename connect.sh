b1f576f9c76e449ed39e7ba054b72b9cd71ddbdb
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-12-02 18:28:41 +0100

    connect: disable trace
    Not really needed for this.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- connect.sh
+++ connect.sh
@@ -1,5 +1,9 @@
-#! /bin/bash -x
+#! /bin/bash
 
+if [[ "${-}" =~ "x" ]]; then
+	export INPUT_TRACE=1
+fi
 docker exec -it \
+	-e "INPUT_TRACE=${INPUT_TRACE:-0}" \
 	"$(docker ps --filter "label=name=mptcp-upstream-virtme-docker" -l --format "{{.ID}}")" \
 	/entrypoint.sh connect "${@}"
