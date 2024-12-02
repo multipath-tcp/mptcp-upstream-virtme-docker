5f2ef50deb9cfac8fc574d0bdc361ed5f91a8f7e
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-12-02 18:48:47 +0100

    container: pass all INPUT_* env var
    Instead of one by one.
    
    Also required, not to reset something already set before.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- connect.sh
+++ connect.sh
@@ -2,7 +2,8 @@
 # SPDX-License-Identifier: GPL-2.0
 DIR="$(dirname "$(realpath -P "${0}")")"
 
+export INPUT_TRACE=0
 if [[ "${-}" =~ "x" ]]; then
-	export INPUT_TRACE=1
+	INPUT_TRACE=1
 fi
 bash "-${-}" "${DIR}/container.sh" /entrypoint.sh connect "${@}"
--- container.sh
+++ container.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
+envs=()
+for env in "${!INPUT_@}"; do
+	envs+=(-e "${env}=${!env}")
+done
+
 docker exec -it \
-	-e "INPUT_TRACE=${INPUT_TRACE:-0}" \
+	"${envs[@]}" \
 	"$(docker ps --filter "label=name=mptcp-upstream-virtme-docker" -l --format "{{.ID}}")" \
 	"${@:-bash}"
