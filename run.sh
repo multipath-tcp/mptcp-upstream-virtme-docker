d2b39ec814e1429b6399f3e3e0cd16d41f602a1a
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-12-02 18:52:13 +0100

    run: pass all INPUT_* env var
    Instead of one by one.
    
    Easier to maintain.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- run.sh
+++ run.sh
@@ -11,6 +11,11 @@ VIRTME_GIT_DIR="$(realpath "$(git rev-parse --git-common-dir)")"
 
 HOME_DIR="$(realpath "$(dirname "${0}")/.home")"
 
+envs=()
+for env in "${!INPUT_@}"; do
+	envs+=(-e "${env}=${!env}")
+done
+
 docker run \
 	-v "${PWD}:${PWD}:rw" \
 	-v "${VIRTME_GIT_DIR}:${VIRTME_GIT_DIR}:ro" \
@@ -19,34 +24,10 @@ docker run \
 	${VIRTME_SYZKALLER_PATH:+ -v "${VIRTME_SYZKALLER_PATH}:/opt/syzkaller:rw"} \
 	${VIRTME_NG_PATH:+ -v "${VIRTME_NG_PATH}:/opt/virtme-ng:ro"} \
 	-w "${PWD}" \
-	-e "INPUT_CLANG" \
-	-e "INPUT_TRACE" \
-	-e "INPUT_NO_BLOCK" \
-	-e "INPUT_RUN_LOOP_CONTINUE" \
-	-e "INPUT_BUILD_SKIP" \
-	-e "INPUT_BUILD_SKIP_PERF" \
-	-e "INPUT_BUILD_SKIP_SELFTESTS" \
-	-e "INPUT_BUILD_SKIP_BPFTESTS" \
-	-e "INPUT_BUILD_SKIP_PACKETDRILL" \
-	-e "INPUT_BUILD_SUFFIX" \
-	-e "INPUT_MAKE_ARGS" \
-	-e "INPUT_RUN_TESTS_ONLY" \
-	-e "INPUT_RUN_TESTS_EXCEPT" \
-	-e "INPUT_SELFTESTS_DIR" \
-	-e "INPUT_SELFTESTS_MPTCP_LIB_EXPECT_ALL_FEATURES" \
-	-e "INPUT_SELFTESTS_MPTCP_LIB_OVERRIDE_FLAKY" \
 	-e "INPUT_PACKETDRILL_NO_SYNC=${VIRTME_PACKETDRILL_PATH:+1}" \
-	-e "INPUT_PACKETDRILL_NO_MORE_TOLERANCE=${INPUT_PACKETDRILL_NO_MORE_TOLERANCE:-${VIRTME_PACKETDRILL_PATH:+1}}" \
+	-e "INPUT_PACKETDRILL_NO_MORE_TOLERANCE=${VIRTME_PACKETDRILL_PATH:+1}" \
 	-e "INPUT_PACKETDRILL_STABLE=${VIRTME_PACKETDRILL_STABLE:-0}" \
-	-e "INPUT_EXPECT_TIMEOUT" \
-	-e "INPUT_EXTRA_ENV" \
-	-e "INPUT_HOSTNAME" \
-	-e "INPUT_CPUS" \
-	-e "INPUT_RAM" \
-	-e "INPUT_GCOV" \
-	-e "INPUT_NET_BRIDGES" \
-	-e "INPUT_MAC_ADDRESS_PREFIX" \
-	-e "INPUT_VSOCK_CID" \
+	"${envs[@]}" \
 	-e "VIRTME_ARCH" \
 	-e "COMPILER" \
 	--privileged \
