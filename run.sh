93dcc8a32ca17296e604c750292d512535bf78d4
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-12-05 14:20:16 +0100

    run: mount iproute2 dir if set/available
    Either if the VIRTME_IPROUTE2_PATH env var is set (full path required)
    or if the ../iproute2 directory is available.
    
    Useful to test dev versions without having to restart the VM each time.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ VIRTME_INTERACTIVE=""
 test -t 1 && VIRTME_INTERACTIVE="-t"
 [ "${VIRTME_NO_INTERACTIVE}" != 1 ] && VIRTME_INTERACTIVE="-it"
 [ -z "${VIRTME_SYZKALLER_PATH}" ] && [ -d ../syzkaller ] && VIRTME_SYZKALLER_PATH="$(realpath "../syzkaller")"
+[ -z "${VIRTME_IPROUTE2_PATH}" ] && [ -d ../iproute2 ] && VIRTME_IPROUTE2_PATH="$(realpath "../iproute2")"
 
 # host is different if worktree are used
 VIRTME_GIT_DIR="$(realpath "$(git rev-parse --git-common-dir)")"
@@ -31,6 +32,7 @@ docker run \
 	-v "${HOME_DIR}:/root" \
 	-v "/etc/localtime:/etc/localtime:ro" \
 	${VIRTME_SYZKALLER_PATH:+ -v "${VIRTME_SYZKALLER_PATH}:/opt/syzkaller:rw"} \
+	${VIRTME_IPROUTE2_PATH:+ -v "${VIRTME_IPROUTE2_PATH}:${VIRTME_IPROUTE2_PATH}:rw"} \
 	${VIRTME_NG_PATH:+ -v "${VIRTME_NG_PATH}:/opt/virtme-ng:ro"} \
 	-w "${PWD}" \
 	-e "INPUT_PACKETDRILL_NO_SYNC=${VIRTME_PACKETDRILL_PATH:+1}" \
