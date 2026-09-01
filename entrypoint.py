57795d021f1f7cc8199a0568152b6df12b2028da
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2026-09-01 16:48:25 +0200

    perf: remove symlink to log dir
    Logs will not be available in the results, and this symlink will be
    broken there.
    
    It was added because it was conveniant, but fine without it.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- entrypoint.py
+++ entrypoint.py
@@ -210,12 +210,6 @@ class Entrypoint:
         os.makedirs(new_path, exist_ok=True)
         os.symlink(new, latest)
 
-        # symlink to the log dir
-        logs = os.path.join(self.reg_dir, "logs")
-        if os.path.islink(logs):
-            os.remove(logs)
-        os.symlink(os.path.relpath(self.log_dir, self.reg_dir), logs)
-
         # copy stats and artifacts
         shutil.copytree(
             os.path.join(self.log_dir, "stats"), os.path.join(new_path, "stats")
